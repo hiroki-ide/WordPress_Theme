b4f97052dfdaeaa88f0ef141a8ef3f2ee2bf2e46
hiroki-ide <ide.hiroki@gmail.com>
2014-09-12 02:05:21 +0900

    Fix_CopyRight

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
       <div id="footer" class="container">
-       (c) 2014 <?php if(date("Y")!= 2014) echo date("-Y") ?> ShishakuLabo.com
+       (c) 2014 <?php if(date("Y")!= 2014) echo date("-Y") ?> <?php bloginfo('name'); ?>
       </div>
   <?php wp_footer(); ?>
   </body>
